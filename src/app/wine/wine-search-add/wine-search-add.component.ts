fbd8cb407492eac78f981e8c0530250f5e4083d8
CJRivera <carisua@verizon.net>
2022-06-11 10:41:58 -0400

    wine-searcher

--- src/app/wine/wine-search-add/wine-search-add.component.ts
+++ src/app/wine/wine-search-add/wine-search-add.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { catchError, EMPTY } from 'rxjs';
 import { Profile } from 'src/app/models/Profile';
+import { VinomioCollectionService } from 'src/app/services/vinomio-collection.service';
 
 @Component({
   selector: 'app-wine-search-add',
@@ -12,6 +15,9 @@ export class WineSearchAddComponent implements OnInit {
   @Input() navData!: any
 
   @Output() navEvent =  new EventEmitter<{}>();
+
+  addWineForm!:FormGroup
+
   formats: { id: string; name: string }[] = [
     { id: '750ml', name: '750ml' },
     { id: '1.5L', name: '1.5L' },
@@ -27,25 +33,65 @@ export class WineSearchAddComponent implements OnInit {
     {id:2024, name:"2024"},
     {id:2025, name:"2025"},
   ]
-  constructor() { }
+  constructor(
+    private collectionService:VinomioCollectionService
+  ) { }
 
   ngOnInit(): void {
+
     const vintage = this.navData.data.wine.Vintages.filter((i:any)=>i.id ==this.navData.data.vintageId)
-    console.log(vintage)
     if(vintage && vintage.length > 0)
       this.vintages = [{id:vintage[0].year, name:`${vintage[0].year}`}]
-      console.log(this.vintages)
+
+    this.addWineForm = new FormGroup({
+      purchasedOn : new FormControl('',[Validators.required, Validators.pattern('[0-1]{2}/[0-9]{2}/[0-9]{4}')]),
+      deliverBy : new FormControl('',[Validators.required, Validators.pattern('[0-1]{2}/[0-9]{2}/[0-9]{4}')]),
+      numOfBottles : new FormControl('1',[Validators.required, Validators.pattern('[0-9]+')]),
+      price : new FormControl('',[Validators.required]),
+      format : new FormControl("750ml",[Validators.required]),
+      state : new FormControl(false)
+    })
   }
   onBack(){
     this.navEvent.emit({history:this.navData.history})
   }
-  onAdd(){
-    this.navEvent.emit({})
+  onSubmit(){
+    const data:any[] = [{
+        //vintageId: this.navData.data.vintageId,
+        vintage: this.vintages[0]?.name,
+        wineId: this.navData.data.wine.id,
+        cellarId: this.profile.cellar_id,
+        price: this.addWineForm.value.price,
+        bottleCount: this.addWineForm.value.numOfBottles,
+        bottleSize: this.addWineForm.value.format,
+        /*locationId: 0,
+        acquiringSourceId: 0,
+        allocationEventId: this.allocationEvent.id,*/
+        purchasedOn:this.addWineForm.value.price,
+        deliverBy: this.addWineForm.value.price,
+        statusId:  this.addWineForm.value.state ? 'allocated' : 'pending'
+      }];
+      //console.log(data)
+      
+    if(data.length > 0){
+      this.collectionService.add(data).pipe(
+        catchError((err) => { console.debug(err); return EMPTY})
+      )
+      .subscribe((r) =>this.navEvent.emit({}));
+    }
   }
-  updateStatus(){}
+  updateStatus(){
+    this.addWineForm.patchValue({"state" : !this.addWineForm.value.state})
+  }
 
   public get WineName():string{
     return `${this.navData.data.wine.name}`
+  }
+  public get Region():string{
+    return `${this.navData.data.wine.Region.name}`
+  }
+  public get MasterVarietal():string{
+    return `${this.navData.data.wine.MasterVarietal.name}`
   }
 /**
  * 
