8686cd48a4e7a0cadbaf3109d0bd24a7c4fc3255
CJRivera <carisua@verizon.net>
2022-11-29 21:39:09 -0500

    profile fixes

--- src/app/profile/profile-cellar/profile-cellar.component.ts
+++ src/app/profile/profile-cellar/profile-cellar.component.ts
@@ -1,3 +1,4 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import {
   FormArray,
@@ -84,29 +85,45 @@ export class ProfileCellarComponent implements OnInit {
         owner: `${this.owner.firstName} ${this.owner.lastName}`,
       });
       Object.keys(this.userCellar.attributes).forEach((attrKey) => {
-        const data = createObject(
-          attrKey,
-          new FormControl((<any>this.userCellar.attributes)[attrKey], [])
-        );
-        const group = this.fb.group(data) as FormGroup;
-        (<FormArray>this.cellarForm.get('attributes')).push(group);
+        let data:any
+        let container:any
+        if (Array.isArray((<any>this.userCellar.attributes)[attrKey])) {
+            const items:[] = (<any>this.userCellar.attributes)[attrKey];
+            this.AttributesFormGroup.addControl(attrKey, new FormArray([]));
+            const formArray = this.AttributesFormGroup.get(attrKey) as FormArray;
+            items.forEach(c =>{
+              const group = this.fb.group(c)
+              formArray.push(group)
+            })            
+        } else {
+            data = createObject(
+            attrKey,
+            new FormControl((<any>this.userCellar.attributes)[attrKey], [])
+          );
+          container = this.fb.group(data) as FormGroup;
+          (<FormArray>this.cellarForm.get('attributes')).push(container);
+        }
       });
     });
+    //console.log(this.cellarForm)
   }
   public get Attributes(): FormArray {
     return <FormArray>this.cellarForm.get('attributes');
   }
   public get SubAttributes(): FormArray {
-    //console.log(<FormArray>this.AttributesFormGroup.get('partition'))
-    return <FormArray>this.AttributesFormGroup.get('partition')
+    return <FormArray>this.AttributesFormGroup.get('partition');
   }
   public get AttributesFormGroup(): FormGroup {
     return <FormGroup>this.Attributes.controls[0];
   }
-  public AttributesChildFormGroup(slug:string, index?:any): FormGroup {
-    const formArray:FormArray = (<FormArray>this.AttributesFormGroup.get(slug))
-    const formGroup:FormGroup = (<FormGroup>formArray.at(index))
-    return formGroup
+  public AttributesItems(slug: string):any[] {
+    const array:FormArray = <FormArray>this.AttributesFormGroup.get(slug)
+    return <any>array.controls
+  }
+  public AttributesChildFormGroup(slug: string, index?: any): FormGroup {
+    const formArray: FormArray = <FormArray>this.AttributesFormGroup.get(slug);
+    const formGroup: FormGroup = <FormGroup>formArray.at(index);
+    return formGroup;
   }
   getOwner(): User {
     return this.owner;
@@ -116,8 +133,31 @@ export class ProfileCellarComponent implements OnInit {
       ? this.userCellar.Users.length
       : 0;
   }
+  isCollapsable(target:any){
+    //form-check-input
+    const isChecked = (<any>(
+      (<HTMLInputElement>target).getElementsByClassName('form-check-input')[0]
+    ))?.checked || false
+    return isChecked
+  }
+  isEmpty(attribute:CellarAttribute){
+    const items:any[] =Array.isArray(attribute.value) ? Array(attribute.value) : []
+    //console.log(`${attribute.key} ${items.length}`)
+    return items.length > 0 ? false : true
+  }
+  
   onSubmit() {
-    console.log(this.cellarForm.get('attributes')?.value)
+    const id:number = this.cellarForm.get('id')?.value
+    //console.log(this.cellarForm.value[0])
+    if(id){
+      console.log(id)
+      const data:{attributes:any} = {attributes:this.Attributes.value[0]}
+      this.cellarService.put(id,data).subscribe(resp => {
+        if(resp.ok){
+          console.log("done")
+        }
+      })
+    }
   }
   _setAddButtonStatus() {
     this._showAddStatus = !this._showAddStatus;
@@ -133,13 +173,9 @@ export class ProfileCellarComponent implements OnInit {
     );
   }
   showAdditionalAttributes(child: any, parent: any) {
-    const formArraySlug = parent.firstChild.getElementsByTagName('button')[0]?.name
-    //if(formArraySlug)
-    //  this.onAddPartition(formArraySlug)
-
-    const target: HTMLElement = <HTMLElement>(
-      (<HTMLElement>parent).lastElementChild?.firstChild?.firstChild
-    );
+    
+    const formArraySlug = parent.firstChild.getElementsByTagName('button')[0]?.name;
+    const target: HTMLElement = <HTMLElement>(<HTMLElement>parent).lastElementChild?.getElementsByTagName("div")[0]
     const isExpanded: boolean = target.classList.contains('expanded');
     const isChecked = (<any>(
       (<HTMLInputElement>parent).getElementsByClassName('form-check-input')[0]
@@ -157,78 +193,70 @@ export class ProfileCellarComponent implements OnInit {
     }
   }
   onSelectedAttribute(attr: any) {
-   
     this._setAddButtonStatus();
     this.features[this.features.length - 1].key = attr.key;
     this.features[this.features.length - 1].readonly = true;
     this.features[this.features.length - 1].type = this.CellarAttributes.find(
-      (i) => i.key == attr.key
+      (i) => slugify(i.key) == slugify(attr.key)
     )?.type;
     this.features[this.features.length - 1].slug = slugify(attr.key);
     const newAttr = this.features[this.features.length - 1];
-     //check if control exists! otherwise add one.
-     const control = this.AttributesFormGroup.get(newAttr.key);
-     if (!control)
-       switch (newAttr.type) {
-         case 'Toggle': {
-           this.AttributesFormGroup.addControl(newAttr.slug,new FormArray([]));
-           break;
-         }
-         default: {
-           this.AttributesFormGroup.addControl(newAttr.key,new FormControl('', []));
-           break;
-         }
-       }
-     else {
-       console.log('???');
-     }
+    //check if control exists! otherwise add one.
+    const control = this.AttributesFormGroup.get(newAttr.slug);
+    if (!control)
+      switch (newAttr.type) {
+        case 'Toggle': {
+          this.AttributesFormGroup.addControl(newAttr.slug, new FormArray([]));
+          break;
+        }
+        default: {
+          this.AttributesFormGroup.addControl(
+            newAttr.slug,
+            new FormControl('', [])
+          );
+          break;
+        }
+      }
+    else {
+      console.log('???');
+    }
   }
-  _onAddRowControl(slug:string, columns:string[]) : FormGroup{
-    console.log(slug)
-    const formArray:FormArray = <FormArray>this.AttributesFormGroup.get(slug)
-    const formGroup:FormGroup = new FormGroup({})
-    columns.forEach(c => {
-      formGroup.addControl(c,new FormControl('',[]))
-    })
-    formArray.push(formGroup)
-    console.log(formGroup)
-    return formGroup
+  _onAddRowControl(slug: string, columns: string[]): FormGroup {
+    console.log(slug);
+    const formArray: FormArray = <FormArray>this.AttributesFormGroup.get(slug);
+    const formGroup: FormGroup = new FormGroup({});
+    columns.forEach((c) => {
+      formGroup.addControl(c, new FormControl('', []));
+    });
+    formArray.push(formGroup);
+    console.log(formGroup);
+    return formGroup;
   }
   onAddDistribution() {
-    //this.distributions.push({ country: '', count: '' });
-    const columns:string[]=['country','threshold']
-    const formGroup = this._onAddRowControl('distribution',columns)
-    this.distributions.push(formGroup.value)
+    const columns: string[] = ['country', 'threshold'];
+    const formGroup = this._onAddRowControl('distribution', columns);
+    this.distributions.push(formGroup.value);
   }
-  onAddPartition(slug:any) {
-    const columns:string[]=['name', 'segment', 'count']
-    const formGroup = this._onAddRowControl('partition',columns)
-    /*
-    const formArray:FormArray = <FormArray>this.AttributesFormGroup.get(slug)
-    const formGroup:FormGroup = new FormGroup({})
-    columns.forEach(c => {
-      formGroup.addControl(c,new FormControl('',[]))
-    })
-    formArray.push(formGroup)*/
-    //console.log(this.partitions)
-    this.partitions.push(formGroup.value)
-    //console.log(this.cellarForm)
+  onAddPartition(slug: any) {
+    const columns: string[] = ['name', 'segment', 'count'];
+    const formGroup = this._onAddRowControl('partition', columns);
+    this.partitions.push(formGroup.value);
   }
   onAddNewAttribute() {
     this._setAddButtonStatus();
     this.features.push({ key: '', value: '', readonly: false, type: '' });
   }
   onRemoveNewAttribute(attr: any, element: any) {
-    //console.log(element)
-    console.log(attr)
     if ((<HTMLElement>element).classList.contains('expanded'))
       (<HTMLElement>element).classList.replace('expanded', 'collapsed');
     else if ((<HTMLElement>element).classList.contains('collapsed'))
       (<HTMLElement>element).classList.replace('collapsed', 'expanded');
     else {
-      this.features = this.features.filter((x) => x.key != attr.key);
+      this.features = this.features.filter((x) => slugify(x.key) != slugify(attr.key));
       this._showAddStatus = true;
-      (<FormGroup>(<FormArray>this.cellarForm.get('attributes')).controls[0]).removeControl(attr.slug)
+      (<FormGroup>(
+        (<FormArray>this.cellarForm.get('attributes')).controls[0]
+      )).removeControl(attr.slug);
     }
 
     const child = (<HTMLElement>element).parentElement?.parentElement
@@ -243,11 +271,12 @@ export class ProfileCellarComponent implements OnInit {
   _initAttributes() {
     this.features = Object.keys(this.userCellar.attributes).map(
       (key: string) => {
+        const ca:any = this.CellarAttributes.find((i) => slugify(i.key) == slugify(key))
         return {
-          key: key,
+          key: ca?.key,
           value: (<any>this.userCellar.attributes)[key],
           readonly: true,
-          type: this.CellarAttributes.find((i) => i.key == key)?.type,
+          type: this.CellarAttributes.find((i) => slugify(i.key) == slugify(key))?.type,
           slug: slugify(key),
         };
       }
